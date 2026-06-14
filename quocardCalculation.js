0e64dc9a25fb6590c9214b3f54113c5765486c75
RK-works617 <ryosuke.koide0327@gmail.com>
2026-06-14 14:44:07 +0900

    Create quocardCalculation.js

---
+++ quocardCalculation.js
@@ -0,0 +1,13 @@
+function quocardCalculation(e) {
+  let ss=SpreadsheetApp.getActiveSpreadsheet();
+  let sheet=ss.getSheetByName("記録簿");
+  let date=e.values[1];
+  let inc=Number(e.values[2]);
+  let dec=Number(e.values[3]);
+  let last=sheet.getLastRow()+1;
+  sheet.getRange(last,1).setValue(date);
+  sheet.getRange(last,2).setValue(inc);
+  sheet.getRange(last,3).setValue(dec);
+  let previous=sheet.getRange(last-1,4).getValue();
+  sheet.getRange(last,4).setValue(previous+inc-dec); 
+}
